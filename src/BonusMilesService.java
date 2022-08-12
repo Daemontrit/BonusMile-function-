3eee8a292c990cae9785ab94114ebd415b0c7ad0
Daemontrit <slavikk1999@yandex.ru>
2022-08-12 17:01:03 +0300

    refactor code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int cost){
-        int dopMile=cost/20;
+    public int calculate(int cost) {
+        int dopMile = cost / 20;
         return dopMile;
     }
 }
